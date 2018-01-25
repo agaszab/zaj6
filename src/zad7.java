9c17cb37ca4483e3c0b6bb51561d615ac001a98d
agatito <agatito@yopmail.com>
2018-01-25 20:02:50 +0100

    zajecia

--- src/zad7.java
+++ src/zad7.java
@@ -2,18 +2,22 @@ public class zad7 {
 
     public static void main(String[] args) {
 
+        boolean podzielna=false;
         int x=20;
 
 
 
-        for (int i=1; i<=20; i++) {
+        for (int i=20; ; i++) {
+
+            if (dividedBy20(i)){  // metoda sprawdzająca podzielność przez 20, 19 18 itp
+                System.out.println(i);
+                break;
+            }
 
 
-            while (x%i==0)
-x++;
         }
 
 
-        System.out.println(x);
+
     }
     }
